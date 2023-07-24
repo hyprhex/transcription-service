118721d1d71d71f6b08618cc0f459ed05f7cde57
EL Father <shadaw2008@gmail.com>
2023-07-24 16:35:44 +0100

    Update main.py
    make it easy and short

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import typer
 import assemblyai as aai
 
 app = typer.Typer()
-
+#how to use 
+#python main.py --link (file link or path)
 @app.command()
-def transcript(key: str = typer.Option(), link: str = typer.Option()):
-    aai.settings.api_key = key
+def transcript( link: str = typer.Option()):
+    aai.settings.api_key = "63da4deac437496784f2e748f4a86e00"
     transcriber = aai.Transcriber()
     transcriptText = transcriber.transcribe(link)
     if transcriptText:
-        with open('transcript.txt', 'w') as file:
+        with open(link+'.txt', 'w') as file:
             file.write(transcriptText.text)
         print("Done")
 
