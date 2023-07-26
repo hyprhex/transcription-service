6ef7b5a32e48dd5cf7c36589ec55bffcaa91ce83
Salah Eddine Mebkhouti <124271531+AIpill@users.noreply.github.com>
2023-07-26 23:10:19 +0100

    remove API KEY

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = typer.Typer()
 #python main.py --link (file link or path)
 @app.command()
 def transcript( link: str = typer.Option()):
-    aai.settings.api_key = "63da4deac437496784f2e748f4a86e00"
+    aai.settings.api_key = "YOUR_API_KEY"
     transcriber = aai.Transcriber()
     transcriptText = transcriber.transcribe(link)
     if transcriptText:
